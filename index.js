dddca1e22b07721ab6865ce066bcc1f92125ee9e
NTPN <ntpn294@gmail.com>
2024-05-06 09:24:45 +0200

    moved functions to index.js

--- index.js
+++ index.js
@@ -13,11 +13,20 @@ const result = {
 };
 // ====================functions
 
-import {
-    isValidEmailAddress,
-    isValidPassword,
-    isValidPhoneNumber
-} from './validators.js';
+function isValidEmailAddress(email) {
+    const emailRegex = /^[a-zA-Z]{1}\.[a-zA-Z]{2,}@([a-zA-Z]{2,})\.([a-zA-Z]{2,3})$/;
+    return emailRegex.test(email);
+}
+
+function isValidPassword(password) {
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/;
+    return passwordRegex.test(password);
+}
+
+function isValidPhoneNumber(phoneNumber) {
+    const phoneRegex = /^06[- ]?\d{8}$/;
+    return phoneRegex.test(phoneNumber);
+}
 
 //========================= routes
 app.get('/', (req, res) => {
